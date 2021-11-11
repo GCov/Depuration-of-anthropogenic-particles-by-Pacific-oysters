3278bd49e126dcade5e40ee8e57e3f5b93c8c5d5
GCov <GCov@users.noreply.github.com>
2021-11-11 11:36:38 -0500

    Nov. 11, 2021

--- Updated Figures.R
+++ Updated Figures.R
@@ -12,109 +12,68 @@ theme1 <-
     panel.grid = element_blank()
   )
 
-#### Size Plots ####
+#### Size and Colours plot ####
 
-oysters_size_plot <-
-  ggplot(oysterprop, aes(x=sampleday, y=100*prop, fill = size.cat)) + 
-  geom_col(size = 0.5, colour = "black") + 
-  xlab("Sampling Day") + 
-  ylab("Percent") +
-  guides(fill=guide_legend(title="Particle size category \n (microns)")) +
-  theme_bw() +
-  scale_fill_manual(values = c("grey25", "grey50", "grey75")) +
-  theme1 +
-  theme(plot.margin = margin(0.5,0,0,0, unit = "cm"),
-        legend.position = "none") +
-  scale_x_discrete(expand = c(0,0.5)) + 
-  scale_y_continuous(expand = c(0.03,0))
+daylabs <- c("Day 0", 
+             "Day 1", 
+             "Day 3", 
+             "Day 5", 
+             "Day 10")
+names(daylabs) <- c(0, 1, 3, 5, 10)
 
-water_size_plot <-
-  ggplot(waterprop3, aes(x=day, y=100*mean, fill = size.cat)) + 
-  geom_col(size = 0.5, colour = "black") + 
-  xlab("Sampling day") + 
-  ylab("Percent") +
-  guides(fill=guide_legend(title="Particle size category \n (microns)")) +
-  theme_bw() +
-  scale_fill_manual(values = c("grey25", "grey50", "grey75")) +
-  theme1 +
-  theme(plot.margin = margin(0.5,0,0,0, unit = "cm"),
-        legend.position = "none") +
-  scale_x_discrete(expand = c(0,0.5)) + 
-  scale_y_continuous(expand = c(0.03,0))
+depwater5$size.cat <- factor(depwater5$size.cat,
+                             levels = c("100-500",
+                                        "500-1000",
+                                        "1000-5000"))
 
-algae_size_plot <- 
-  ggplot(algaeprop, aes(x=species, y=100*prop, fill = size.cat)) + 
-  geom_col(size = 0.5, colour = "black") + 
-  xlab("") + 
-  ylab("Percent") +
-  guides(fill=guide_legend(title="Particle size category \n (microns)")) +
-  theme_bw() +
-  scale_fill_manual(values = c("grey25", "grey50", "grey75")) +
-  theme1 +
-  theme(plot.margin = margin(0.5,0,0,0, unit = "cm"),
-        axis.text.x = element_text(face = "italic")) +
-  scale_x_discrete(expand = c(0,0.5)) + 
-  scale_y_continuous(expand = c(0.03,0))
-
-grid1 <- plot_grid(water_size_plot, 
-                   algae_size_plot,
-                   oysters_size_plot,
-                   nrow = 3,
-                   labels = c("Water", "Algae", "Oysters"),
-                   label_size = 8,
-                   align = "v",
-                   axis = "l")
-
-tiff("Depuration Size Plots.tiff",
-     width = 14,
-     height = 10,
-     units = "cm",
-     compression = "none",
-     res = 500)
-
-grid1
-
-dev.off()
-
-
-#### Colour Plots ####
-
-water_colour_plot <-
-  ggplot(watertypeprop3, aes(x=day, y=100*mean, fill = variable)) + 
-  geom_col(size = 0.5, colour = "black") + 
-  xlab("Sampling Day") + 
-  ylab("Percent") +
-  guides(fill=guide_legend(title="Particle size category \n (microns)")) +
-  theme_bw() +
+waterplot <- 
+  ggplot(depwater5) +
+  geom_col(aes(x = size.cat,
+               y = finalcount,
+               fill = variable),
+           colour = "black") +
   scale_fill_manual(values = c("steel blue", "grey90", "red", "turquoise")) +
-  theme1 + 
-  theme(plot.margin = margin(0.5,0,0,0, unit = "cm"),
-        legend.position = "none") +
-  scale_x_discrete(expand = c(0,0.5)) + 
-  scale_y_continuous(expand = c(0.03,0))
+  labs(x = expression(paste("Particle size category ("*mu*"m)")),
+       y = "Particle count") +
+  facet_wrap(~day,
+             labeller = labeller(day = daylabs),
+             nrow = 1) +
+  theme1 +
+  theme(legend.position = "none",
+        axis.text.x = element_text(angle = 20, hjust = 1))
 
-algae_colour_plot <-
-  ggplot(algaetypeprop, aes(x=species, y=100*prop, fill = variable)) + 
-  geom_col(size = 0.5, colour = "black") + 
-  xlab("") + 
-  ylab("") +
-  guides(fill=guide_legend(title="Particle size category \n (microns)")) +
-  theme_bw() +
+algae5$size.cat <- factor(algae5$size.cat,
+                          levels = c("100-500",
+                                     "500-1000",
+                                     "1000-5000"))
+
+algaeplot <-
+  ggplot(algae5) +
+  geom_col(aes(x = size.cat,
+               y = finalcount,
+               fill = variable),
+           colour = "black") +
   scale_fill_manual(values = c("steel blue", "grey90", "red", "turquoise")) +
-  theme1 + 
-  theme(plot.margin = margin(0.5,0,0,0, unit = "cm"),
-        legend.position = "none",
-        axis.text.x = element_text(angle = 45, hjust = 1, face = "italic")) +
-  scale_x_discrete(expand = c(0,0.5)) + 
-  scale_y_continuous(expand = c(0.03,0))
+  labs(x = "",
+       y = "Particle count") +
+  facet_wrap(~species,
+             ncol = 1) +
+  theme1 +
+  theme(legend.position = "none",
+        strip.text = element_text(face = "italic"),
+        axis.text.x = element_text(angle = 20, hjust = 1))
 
-oysters_colour_plot <-
-  ggplot(oystertypeprop, aes(x = sampleday, y = 100 * prop, fill = variable)) +
-  geom_col(size = 0.5, colour = "black") +
-  xlab("Sampling Day") +
-  ylab("Percent") +
-  guides(fill = guide_legend(title = "Particle size category \n (microns)")) +
-  theme_bw() +
+oysterdep5$size.cat <- factor(oysterdep5$size.cat,
+                              levels = c("100-500",
+                                         "500-1000",
+                                         "1000-5000"))
+
+oysterplot <- 
+  ggplot(oysterdep5) +
+  geom_col(aes(x = size.cat,
+               y = finalcount,
+               fill = variable),
+           colour = "black") +
   scale_fill_manual(
     values = c(
       "Grey16",
@@ -129,36 +88,35 @@ oysters_colour_plot <-
       "Turquoise",
       "White",
       "Yellow"
-    )
-  ) +
-  theme1 + 
-  theme(plot.margin = margin(0.5,0,0,0, unit = "cm")) +
-  scale_x_discrete(expand = c(0,0.5)) + 
-  scale_y_continuous(expand = c(0.03,0))
+    )) +
+  labs(x = "",
+       y = "Particle count") +
+  facet_wrap(~sampleday,
+             labeller = labeller(sampleday = daylabs),
+             nrow = 1) +
+  guides(fill = guide_legend(title = "Particle type")) +
+  theme1 +
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(angle = 20, hjust = 1))
 
-grid2 <- 
-  plot_grid(water_colour_plot, 
-            algae_colour_plot,
-            nrow = 1,
-            labels = c("Water", "Algae"),
-            label_size = 8,
-            align = "h",
-            rel_widths = c(1, 0.6))
+grid1 <- plot_grid(oysterplot, waterplot, 
+                   labels = c("Oysters", "Water"),
+                   rel_heights = c(1, 0.32),
+                   label_size = 8,
+                   ncol = 1)
 
-grid3 <- plot_grid(grid2,
-                   oysters_colour_plot,
-                   nrow = 2,
-                   labels = c("", "Oysters"),
-                   label_size = 8)
-
-tiff("Depuration Colour Plots.tiff",
-     width = 14,
-     height = 17,
+tiff("Particle Categories Plot.tiff",
+     width = 19,
+     height = 16,
      units = "cm",
      compression = "none",
      res = 500)
 
-grid3
+plot_grid(grid1, algaeplot,
+          nrow = 1,
+          rel_widths = c(1, 0.24),
+          labels = c("", "Algae"),
+          label_size = 8)
 
 dev.off()
 
@@ -202,98 +160,4 @@ ggplot() +
 
 dev.off()
 
-
-
-
-daylabs <- c("Day 0", 
-             "Day 1", 
-             "Day 3", 
-             "Day 5", 
-             "Day 10")
-names(daylabs) <- c(0, 1, 3, 5, 10)
-
-waterplot <- 
-  ggplot(depwater5) +
-  geom_col(aes(x = size.cat,
-               y = finalcount,
-               fill = variable),
-           colour = "black") +
-  scale_fill_manual(values = c("steel blue", "grey90", "red", "turquoise")) +
-  labs(x = expression(paste("Particle size category ("*mu*"m)")),
-       y = "Particle count") +
-  facet_wrap(~day,
-             labeller = labeller(day = daylabs),
-             ncol = 1) +
-  theme1 +
-  theme(legend.position = "none")
-
-algaeplot <-
-  ggplot(algae5) +
-  geom_col(aes(x = size.cat,
-               y = finalcount,
-               fill = variable),
-           colour = "black") +
-  scale_fill_manual(values = c("steel blue", "grey90", "red", "turquoise")) +
-  labs(x = "",
-       y = "Particle count") +
-  facet_wrap(~species,
-             nrow = 1) +
-  theme1 +
-  theme(legend.position = "none",
-        strip.text = element_text(face = "italic"))
-
-
-oysterdep6 <- subset(oysterdep5, finalcount > 0)
-
-oysterdep6$variable <- as.character(oysterdep6$variable)
-oysterdep6$variable <- as.factor(oysterdep6$variable)
-
-oysterplot <- 
-  ggplot(oysterdep6) +
-  geom_col(aes(x = size.cat,
-               y = finalcount,
-               fill = variable),
-           colour = "black") +
-  scale_fill_manual(
-    values = c(
-      "Grey16",
-      "Steel Blue",
-      "Saddle Brown",
-      "Grey90",
-      "Grey70",
-      "Forest Green",
-      "Orange",
-      "Pink",
-      "Red",
-      "Turquoise",
-      "White",
-      "Yellow"
-    )) +
-  labs(x = expression(paste("Particle size category ("*mu*"m)")),
-       y = "") +
-  facet_wrap(~sampleday,
-             labeller = labeller(sampleday = daylabs),
-             ncol = 1) +
-  guides(fill = guide_legend(title = "Particle type")) +
-  theme1
-
-grid1 <- plot_grid(waterplot, oysterplot,
-                   labels = c("Water", "Oysters"),
-                   rel_widths = c(0.7, 1),
-                   label_size = 8)
-
-tiff("Particle Categories Plot.tiff",
-     width = 19,
-     height = 30,
-     units = "cm",
-     compression = "none",
-     res = 500)
-
-plot_grid(grid1, algaeplot,
-          nrow = 2,
-          rel_heights = c(1, 0.25),
-          labels = c("", "Algae"),
-          label_size = 8)
-
-dev.off()
 
